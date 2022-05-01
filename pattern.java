3dc462e732531a5f6c74b403417ddc91f00675a7
Nilesh <gautamnilesh@gmail.com>
2022-05-01 17:19:49 +0530

    pattern is completely working

---
+++ pattern.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class patten {
+    public static void main(String args[]) {
+       Scanner sc=new Scanner(System.in);
+       System.out.println("Enter any no :");
+
+      int n=sc.nextInt();
+
+      System.out.println("Enter any no :");
+      int m=sc.nextInt();
+      
+      //hollow regtangle
+
+        for(int i=1; i<=n; i++) {
+
+            for(int j=1; j<=m; j++){
+                if(i==1 || j==1 || i==n || j==m) {
+                    System.out.print("*");
+                } else {
+                  System.out.print(" ");
+                }
+            }
+            System.out.println();
+
+        }
+       
+
+
+    }
+
+}
