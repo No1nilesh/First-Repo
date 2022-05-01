9fb660c9e2029687cf9632ea24520d30f3c5750d
Nilesh <gautamnilesh@gmail.com>
2022-05-01 18:05:35 +0530

    initial commit

---
+++ loops.java
@@ -0,0 +1,18 @@
+//import java.util.*;
+public class loops {
+    public static void main(String args[]) {
+    // While loop
+     int i=12;
+     do  {
+       System.out.println("mes college");
+      
+     } while(i<11);
+
+     
+
+    } 
+
+    
+
+    }
+
